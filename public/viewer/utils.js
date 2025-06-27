6d4f4eb03d4fd9c8002f35a081e0540ad7d9efd7
BangNyampahin <nyampahbersama6@gmail.com>
2025-06-27 11:13:57 +0700

    Update utils.js

--- public/viewer/utils.js
+++ public/viewer/utils.js
@@ -1,1 +1,11 @@
-
+export async function getMetadataFromNFT(contract, tokenId) {
+  try {
+    const url = `/api/fetch-metadata?contract=${contract}&tokenId=${tokenId}`;
+    const res = await fetch(url);
+    const json = await res.json();
+    return json;
+  } catch (e) {
+    console.error("Gagal fetch metadata:", e);
+    return null;
+  }
+}
