a743c9e8c72737030aaae9340f148edfb0379539
BangNyampahin <nyampahbersama6@gmail.com>
2025-06-27 11:13:28 +0700

    Update index.js

--- public/viewer/index.js
+++ public/viewer/index.js
@@ -1,1 +1,21 @@
+import { getMetadataFromNFT } from './utils.js';
+import { renderNFTLayout } from './nftRenderer.js';
 
+window.onload = async () => {
+  const urlParams = new URLSearchParams(window.location.search);
+  const contract = urlParams.get('contract');
+  const tokenId = urlParams.get('tokenId');
+
+  if (!contract || !tokenId) {
+    document.body.innerHTML = "<h2>NFT tidak ditemukan 😢</h2>";
+    return;
+  }
+
+  const metadata = await getMetadataFromNFT(contract, tokenId);
+  if (!metadata) {
+    document.body.innerHTML = "<h2>Metadata gagal dimuat 😭</h2>";
+    return;
+  }
+
+  renderNFTLayout(metadata);
+};
