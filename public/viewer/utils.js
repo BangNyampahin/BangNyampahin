4606212127c4e48c2af2e4d93b6b8665aead2e34
BangNyampahin <nyampahbersama6@gmail.com>
2025-06-27 11:49:02 +0700

    Update utils.js

--- public/viewer/utils.js
+++ public/viewer/utils.js
@@ -1,11 +1,18 @@
 export async function getMetadataFromNFT(contract, tokenId) {
   try {
-    const url = `/api/fetch-metadata?contract=${contract}&tokenId=${tokenId}`;
+    const url = `https://api.thirdweb.com/v1/nft/${contract}/${tokenId}/metadata`;
     const res = await fetch(url);
     const json = await res.json();
-    return json;
-  } catch (e) {
-    console.error("Gagal fetch metadata:", e);
+
+    if (json?.metadata_uri?.startsWith('ipfs://')) {
+      const ipfsUrl = json.metadata_uri.replace('ipfs://', 'https://ipfs.io/ipfs/');
+      const data = await fetch(ipfsUrl).then(res => res.json());
+      return data;
+    }
+
+    return json?.metadata || null;
+  } catch (err) {
+    console.error("Gagal fetch metadata:", err);
     return null;
   }
 }
